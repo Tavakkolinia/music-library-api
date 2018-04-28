796c5c5a9164dead5306246e88e8dd2c2d400749
gergana <gergana_ruse@yahoo.com>
2018-04-28 08:39:43 +0100

    updated

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
+const artist = require('./controllers/Artist');
+const artistController = require('./controllers/Artist');
+const artistId = require('./controllers/Artist');
+
 
 require('dotenv').config({
   path: path.join(__dirname, './settings.env'),
 });
 
 const app = express();
-mongoose.connect(process.env.DATABASE_CONN);
-app.use(bodyParser.json())
-app.get('/', (req, res) => res.send('Hello MongoDb!'));
 
+mongoose.connect(process.env.DATABASE_CONN);
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Hello MongoDb!'));
+app.post('/Artist', artist.post);
+app.get('/Artist', artistController.list);
+app.get('/Artist/:artistId', artistId.get);
+app.put('/Artist/:artistId', artistController.put);
 app.listen(3000, () => console.log('It works!'));
